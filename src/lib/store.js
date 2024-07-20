94c34f396522ecc538a748f66a756b83bd1e6819
Jesse Wei <jesseweijapan@gmail.com>
2024-07-20 11:48:29 +0900

    change default tasks

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,10 +1,10 @@
 import {configureStore, createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
 const defaultTasks = [
-  { id: '0', title: 'Something', state: 'TASK_INBOX' },
-  { id: '1', title: 'Something more', state: 'TASK_INBOX' },
-  { id: '2', title: 'Something else', state: 'TASK_INBOX' },
-  { id: '3', title: 'Something again', state: 'TASK_INBOX' },
+  { id: '1', title: 'Something', state: 'TASK_INBOX' },
+  { id: '2', title: 'Something more', state: 'TASK_INBOX' },
+  { id: '3', title: 'Something else', state: 'TASK_INBOX' },
+  { id: '4', title: 'Something again', state: 'TASK_INBOX' },
 ];
 
 const initialState = {
